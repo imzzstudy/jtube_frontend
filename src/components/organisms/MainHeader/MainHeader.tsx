c627903e19b416a0a3a5dcde38381d523acad7bd
Jung ji hyung <gw03176370@gmail.com>
2023-07-08 19:33:02 +0900

    fix: Input컴포넌트 위치 수정으로 인한 충돌 수정

--- src/components/organisms/MainHeader/MainHeader.tsx
+++ src/components/organisms/MainHeader/MainHeader.tsx
@@ -1,34 +1,34 @@
 import IconButton from "components/molecules/IconButton/IconButton";
 import Image from "components/atoms/Image/Image";
-import Input from "components/atoms/TextField/Input";
+import Input from "components/atoms/Input/Input";
 import Avatar from "components/atoms/Avatar/Avatar";
-import img from "../../../images/logo.png"
+import img from "../../../images/logo.png";
 
 import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
-    return (
-        <div className={classes.header_wrap}>
-                <div className={classes.start}>
-                    <IconButton icon="menu"/>
-                    <div style={{"padding": "12px"}}>
-                        <Image src={img}/>
-                    </div>
-                </div>
-                <div className={classes.center}>
-                    <div style={{"padding": "12px"}}>
-                        <Input type="text"/>
-                    </div>
-                    <IconButton icon="search"/>
-                </div>
-                <div className={classes.end}>
-                    <IconButton icon="notifications"/>
-                    <div style={{"padding":"12px"}}>
-                        <Avatar size="32px"/>
-                    </div>
-                </div>
-            </div>
-    )
-}
+  return (
+    <div className={classes.header_wrap}>
+      <div className={classes.start}>
+        <IconButton icon="menu" />
+        <div style={{ padding: "12px" }}>
+          <Image src={img} />
+        </div>
+      </div>
+      <div className={classes.center}>
+        <div style={{ padding: "12px" }}>
+          <Input type="text" />
+        </div>
+        <IconButton icon="search" />
+      </div>
+      <div className={classes.end}>
+        <IconButton icon="notifications" />
+        <div style={{ padding: "12px" }}>
+          <Avatar size="32px" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MainHeader;
